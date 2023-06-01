6b72f8b015bee87b1e574ecc87895043fe4bef3b
김주형 <wnguddl96@knou.ac.kr>
2023-06-01 14:54:17 +0900

    refactor(PostsService): Posts 클래스에서 DB로 접근한 PostsRepository 쿼리 실행 결과를 모두 반환해주는 findAllDesc() 메서드 추가

--- src/main/java/com/joohyeong/admin/service/PostsService.java
+++ src/main/java/com/joohyeong/admin/service/PostsService.java
@@ -2,13 +2,16 @@ package com.joohyeong.admin.service;
 
 import com.joohyeong.admin.domain.posts.Posts;
 import com.joohyeong.admin.domain.posts.PostsRepository;
+import com.joohyeong.admin.web.dto.PostsListResponseDto;
 import com.joohyeong.admin.web.dto.PostsResponseDto;
 import com.joohyeong.admin.web.dto.PostsSaveRequestDto;
 
 import com.joohyeong.admin.web.dto.PostsUpdateRequestDto;
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -38,4 +41,13 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)// .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+
 }
