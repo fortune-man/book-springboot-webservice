0f5aa11bae502b88f70425ff8e647a46b1035d7d
김주형 <wnguddl96@knou.ac.kr>
2023-06-01 16:05:45 +0900

    refactor(PostApiController) : 삭제 id 리턴하는 컨트롤러 추가

--- src/main/java/com/joohyeong/admin/web/PostApiController.java
+++ src/main/java/com/joohyeong/admin/web/PostApiController.java
@@ -5,6 +5,7 @@ import com.joohyeong.admin.web.dto.PostsResponseDto;
 import com.joohyeong.admin.web.dto.PostsSaveRequestDto;
 import com.joohyeong.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +29,16 @@ public class PostApiController {
        return postsService.update(id, requestDto);
    }
 
-   @GetMapping("/api/v1/posts/{id}")
-   public PostsResponseDto findById (@PathVariable Long id) {
-       return postsService.findById(id);
-   }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 
 }
