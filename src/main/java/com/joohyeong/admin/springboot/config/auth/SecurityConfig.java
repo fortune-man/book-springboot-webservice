2b819ef57afe99d53cc57b0ad55ed594230ffd0e
김주형 <wnguddl96@knou.ac.kr>
2023-06-05 19:12:18 +0900

    feat(SecurityConfig): 스프링 시큐리티 설정을 활성화하고 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체 등록하는 SecurityConfig 추가

---
+++ src/main/java/com/joohyeong/admin/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.joohyeong.admin.springboot.config.auth;
+
+import com.joohyeong.admin.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    private final OAuth2UserService oAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oAuth2UserService);
+    }
+}
